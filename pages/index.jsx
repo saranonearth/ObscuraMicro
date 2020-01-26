32b8b0ea58ba87c0e3df7aab2fa176b54181830c
Hardik Singh <hardiksingh297@gmail.com>
2020-01-27 00:37:16 +0530

    Can now login via Google account popup.

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,25 @@ import {auth,firebase} from '../lib/firebase';
 const index = () => {
   console.log(auth);
   console.log(firebase);
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const loginHandler = ()=>{
+    firebase.auth().signInWithPopup(provider)
+    .then(()=>{
+      const token = result.credential.accessToken;
+      const user = result.user;
+    }).catch((error)=>{
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+      });
+  }
   return (
     <App>
       <div>
         <h1>Index here bruh</h1>
         <Link href="/game">
-          <button>Login with Google</button>
+          <button onClick={loginHandler}>Login with Google</button>
         </Link>
       </div>
     </App>
