261a562114be81bb19acefb88333f2cfac45b79d
saranonearth <saran.suresh@outlook.com>
2020-01-28 01:03:45 +0530

    Google Auth

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,45 +1,25 @@
 import App from "../components/App";
 import Link from "next/link";
-<<<<<<< HEAD
+import Router from "next/router";
 import { auth, firebase } from "../lib/firebase";
-=======
-import Router from 'next/router';
-import {auth,firebase} from '../lib/firebase';
->>>>>>> c954af9... Game page now opens after authentication is complete.
 
 const index = () => {
-  console.log(auth);
-  console.log(firebase);
-<<<<<<< HEAD
-  const provider = new firebase.auth.GoogleAuthProvider();
   const loginHandler = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
     firebase
       .auth()
       .signInWithPopup(provider)
-      .then(() => {
+      .then(result => {
         const token = result.credential.accessToken;
         const user = result.user;
+        console.log(token, user);
+        Router.push("/game");
       })
       .catch(error => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        const email = error.email;
-        const credential = error.credential;
       });
   };
-=======
-  const loginHandler = ()=>{
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then((result)=>{
-      const token = result.credential.accessToken;
-      const user = result.user;
-      Router.push('/game');
-    }).catch((error)=>{
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-  }
->>>>>>> c954af9... Game page now opens after authentication is complete.
   return (
     <App>
       <div>
