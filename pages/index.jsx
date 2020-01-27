c954af98162ea372752f63ca8d20583ad6a354d8
Hardik Singh <hardiksingh297@gmail.com>
2020-01-27 22:01:08 +0530

    Game page now opens after authentication is complete.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,30 +1,24 @@
 import App from "../components/App";
 import Link from "next/link";
+import Router from 'next/router';
 import {auth,firebase} from '../lib/firebase';
 
 const index = () => {
   console.log(auth);
   console.log(firebase);
-  const provider = new firebase.auth.GoogleAuthProvider();
   const loginHandler = ()=>{
-    firebase.auth().signInWithPopup(provider)
-    .then(()=>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result)=>{
       const token = result.credential.accessToken;
       const user = result.user;
-    }).catch((error)=>{
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = error.credential;
-      });
+      Router.push('/game');
+    });
   }
   return (
     <App>
       <div>
         <h1>Index here bruh</h1>
-        <Link href="/game">
-          <button onClick={loginHandler}>Login with Google</button>
-        </Link>
+        <button onClick={loginHandler}>Google Log in</button>
       </div>
     </App>
   );
