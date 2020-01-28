ea4b2f9b444c34d44f7172caca818f7e374d53eb
Hardik Singh <hardiksingh297@gmail.com>
2020-01-28 23:49:41 +0530

    Nothing.

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,23 @@ import Router from "next/router";
 import { auth, firebase } from "../lib/firebase";
 import { useContext } from "react";
 import Store from "../Store/Context";
+import firestore from 'firebase';
 
 const index = () => {
   const { state, dispatch } = useContext(Store);
   console.log("STATE", state);
   console.log("DISPATCH", dispatch);
+  const addUser = (userId)=>{
+    const db = firebase.firestore();
+    db
+      .collection('user')
+      .doc(userId)
+      .set({
+        Name:'fusionmaster7',
+        Bio:'Nothing',
+        Ques: 0
+      });
+  };
   const loginHandler = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
     firebase
@@ -18,7 +30,7 @@ const index = () => {
         const user = result.user;
         const userId = user.uid;
         console.log(token, user);
-        addData(userId,'Hardik');
+        addUser(userId);
       })
       .catch(error => {
         const errorCode = error.code;
