3e75b22e4ed8b3e91cde002d6713d12d753f1f1b
Hardik Singh <hardiksingh297@gmail.com>
2020-01-28 09:15:02 +0530

    Logs the user

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import App from "../components/App";
 import Link from "next/link";
-<<<<<<< HEAD
 import Router from "next/router";
 import { auth, firebase } from "../lib/firebase";
 
@@ -21,27 +20,6 @@ const index = () => {
         const errorMessage = error.message;
       });
   };
-=======
-import Router from 'next/router';
-import {auth,firebase} from '../lib/firebase';
-
-const index = () => {
-  console.log(auth);
-  console.log(firebase);
-  //To authenticate user when they log in.
-  const loginHandler = ()=>{
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then((result)=>{
-      const token = result.credential.accessToken;
-      const user = result.user;
-      console.log('The current user is ',user.toJSON());
-    }).catch((error)=>{
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-    Router.push('/game');
-  }
->>>>>>> bb7e4dd49a715fed276c5b28dc99d81999dc217a
   return (
     <App>
       <div>
